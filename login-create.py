e57354d3c15277ea5ba83920618bc577074543ec
TanTan1941 <105098069+TanTan1941@users.noreply.github.com>
2025-04-13 23:53:51 -0700

    Update login-create.py

--- login-create.py
+++ login-create.py
@@ -1,12 +1,10 @@
 import bcrypt
 
 FilePath = "userpass.csv"
-
-
 StrUser = input("Please enter your username \n")
-
 StrPass = input("Please enter your password \n")
 StrPassHash = bcrypt.hashpw(StrPass.encode('utf-8'), bcrypt.gensalt())
+DecHash = StrPassHash.decode('utf-8')
 
 with open(FilePath, "a") as file:
-    file.write(f"User:{StrUser} \t Pass:{StrPassHash} \n")
+    file.write(f"{StrUser},{DecHash}\n")
