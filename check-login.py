879643d4de0ba124e054dcd5d48d5d9dc0f30e26
TanTan1941 <105098069+TanTan1941@users.noreply.github.com>
2025-04-16 12:57:31 -0700

    Update check-login.py
    Characters typed for the password portion are now hidden

--- check-login.py
+++ check-login.py
@@ -1,30 +1,31 @@
 import csv
 import bcrypt
+from getpass import getpass
 
 def chkcred(struser, strpass):
     with open("userpass.csv", 'r') as file:
-        
+
         reader = csv.reader(file)
-        
+
         for row in reader:
             if len(row) != 2:
                 continue
             stored_user, stored_hash = row
             if stored_user == struser:
                 if bcrypt.checkpw(strpass.encode('utf-8'), stored_hash.encode('utf-8')):
-                    return True 
-                
+                    return True
+
                 else:
                     return False
-                
+
     return False
 
 username = input("please input your Username \n")
 
-password = input("Please input your Password \n")
+password = getpass("Please input your Password \n")
 
 if chkcred(username, password):
     print(f"Logins match {username} \n")
-    
+
 else:
     print(f"Invald username or password {username}")
